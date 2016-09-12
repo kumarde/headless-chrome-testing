a6c020f009e0f26edffc104c9dceb2140523fcad
Deepak Kumar <kumarde@umich.edu>
2016-09-12 12:14:11 -0700

    adding port from command line as well as changing model of loading

--- network.js
+++ network.js
@@ -8,30 +8,44 @@ var Node = require('tree-node');
 
 const url = require('url')
 
+if (process.argv.length != 6){
+    throw Error('incorrect usage');    
+}
+
 if (process.argv[2] === undefined) {
     throw Error('No headless binary path provided.');
 }
-
-var headless = spawn(process.argv[2], [
-        '--remote-debugging-port=9222']);
 
 var screenshotUrl = process.argv[3] || 'https://paulirish.com';
 var input_hostname = url.parse(screenshotUrl).hostname;
 
 var outputFile = process.argv[4] || "outfile";
 
+var port = process.argv[5];
+
+var headless = spawn(process.argv[2], [
+       '--remote-debugging-port='+port]);
+
+headless.stderr.on('data', (data) => {
+    var err_string = `${data}`;
+    if(err_string.includes('errno=98') != false){
+        console.log(err_string);
+        headless.kill();
+        process.exit(98);    
+    } 
+});
 
 // Dumb timeout for now
 setTimeout(connect, 2000);
 
 function getChromeInstance() {
     return new Promise((res, rej) => {
-            Chrome(function (chromeInstance) {
-                res(chromeInstance);
-                }).on('error', function(err){
-                    console.log(err);    
-                    });
-            });
+        Chrome({port: port}, function (chromeInstance) {
+            res(chromeInstance);
+            }).on('error', function(err){
+                console.log(err);    
+        });
+    });
 }
 
 
@@ -42,22 +56,20 @@ function cleanURL(url){
 function connect() {
     var request_data = {}
     var response_data = []
-    var timeout = 10000;
+    var timeout = 15000;
     var root_domain = screenshotUrl;
     var numConnections = 0;
 
 
     function getResourceTree(instance){
         instance.Page.getResourceTree().then((v) => {
-            
+
             var root_domain = cleanURL(v.frameTree.frame.url);
-            
             var outputTree = new tree(root_domain, request_data[root_domain]); 
              
             var frameTreeQueue = new Queue();
             var count = 0; 
             frameTreeQueue.enqueue(v.frameTree);
-            
             /*
                 frameResourceTree:
                     frame : Object
@@ -73,34 +85,50 @@ function connect() {
                     for(var i = 0; i < current.childFrames.length; ++i){
                         frameTreeQueue.enqueue(current.childFrames[i]);
                         child_url = cleanURL(current.childFrames[i].frame.url);
-                        child_data = undefined
+                        child_data = {error: "No network data available for this resource"};
+                        if(child_url == parent_url){
+                            continue;    
+                        }
                         if(request_data[child_url] !== undefined){
                             child_data = request_data[child_url]; 
                         }
-
+                        count++;
+                        console.log(child_url);
                         outputTree.add(child_url, parent_url, child_data, outputTree.traverseBF); 
                     }
                 }
 
                 for(var i = 0; i < current.resources.length; ++i){
                     resource_url = cleanURL(current.resources[i].url);
-                    if(request_data[resource_url] !== undefined){
-                        if(parent_url == resource_url){
-                            continue;    
-                        }
-                        network = {};
-                        network.numTimesRequests = request_data[resource_url].numberOfTimesRequested;
-                        network.request = request_data[resource_url].data.request;
-                        network.response = {};
-                        if(request_data[resource_url].hasOwnProperty("response")){
-                            network.response.url = request_data[resource_url].response.url;
-                            network.response.headers = request_data[resource_url].response.headers;
-                        }
-                        outputTree.add(resource_url, parent_url, network, outputTree.traverseBF);
+                    protocol = url.parse(resource_url).protocol;
+                    if(protocol !== 'http:' && protocol !== 'https:'){
+                        continue;    
                     }
+                    
+                    child_data = {error: "No network data available for this resource"};
+                    
+                    if(resource_url == parent_url){
+                        continue;
+                    }
+
+                    if(request_data[resource_url] !== undefined){
+                        child_data = {}; 
+                        child_data.numTimesRequests = request_data[resource_url].numberOfTimesRequested;
+                        child_data.request = request_data[resource_url].data.request;
+                        child_data.response = {};
+                        if(request_data[resource_url].hasOwnProperty("response")){
+                            child_data.response.url = request_data[resource_url].response.url;
+                            child_data.response.headers = request_data[resource_url].response.headers;
+                        }
+                    }
+                    count++;
+                    console.log(resource_url);
+                    outputTree.add(resource_url, parent_url, child_data, outputTree.traverseBF);
                 }
             }
-            
+          
+            console.log(numConnections);
+            console.log(count); 
             fs.writeFileSync(outputFile, JSON.stringify(outputTree, null, 4));
 
             headless.kill();
@@ -110,43 +138,47 @@ function connect() {
 
     getChromeInstance().then(instance => {
             instance.Network.responseReceived(function(data){
-                data.response.url = cleanURL(data.response.url);
+                if(data.hasOwnProperty('response')){
+                    data.response.url = cleanURL(data.response.url);
 
-                //This request was sent out for the appropriate response
-                if(data.response.url in request_data){
-                request_data[data.response.url].response = data.response;
+                    //This request was sent out for the appropriate response
+                    if(data.response.url in request_data){
+                        request_data[data.response.url].response = data.response;
+                    }
                 }
-                });
+            });
 
             instance.Network.requestWillBeSent(function(data){
                 //find the appropriate "root"
                 data.request.url = cleanURL(data.request.url);
-
-                if(data.redirectResponse != null && numConnections < 3){
-                delete request_data[root_domain];  
-                root_domain = data.request.url;
-                }
-
+                
                 if(!(data.request.url in request_data)){
-                request_data[data.request.url] = {}
-                request_data[data.request.url].numberOfTimesRequested = 0;
+                    request_data[data.request.url] = {}
+                    request_data[data.request.url].numberOfTimesRequested = 0;
                 }
 
                 request_data[data.request.url].data = data
                 request_data[data.request.url].numberOfTimesRequested += 1;
-
+                
                 ++numConnections;
-                });
-
-            instance.Page.loadEventFired(getResourceTree.bind(null, instance));
+            });
+           
+            setTimeout(getResourceTree.bind(null, instance), timeout);
+            //instance.Page.loadEventFired(getResourceTree.bind(null, instance));
 
             instance.Page.enable();
             instance.Network.enable();
 
-            instance.Network.setUserAgentOverride({userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'});
+            mac_firefox_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:45.0) Gecko/20100101 Firefox/45.0";
+
+            linux_chrome_user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36"
+
+            mac_chrome_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
+
+            instance.Network.setUserAgentOverride({userAgent: mac_chrome_user_agent});
 
             instance.once('ready', () => {
-                    instance.Page.navigate({url: screenshotUrl})
-                    });
+                instance.Page.navigate({url: screenshotUrl})
+            });
     });
 }
