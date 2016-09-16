3f62d8543a0e3a13b7a87d5e747769fb5a9da7f3
root <root@silo2.eecs.umich.edu>
2016-09-16 13:46:52 -0400

    changing to 20s timeout

--- network.js
+++ network.js
@@ -62,7 +62,7 @@ function cleanURL(url){
 function connect() {
     var request_data = {}
     var response_data = []
-    var timeout = 30000;
+    var timeout = 20000;
     var root_domain = screenshotUrl;
     var numConnections = 0;
 
